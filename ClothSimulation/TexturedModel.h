477f8dbbfc3ee978cd5921409980c21570dc216e
jiashiwen <jiashiwen@bigo.sg>
2021-05-15 15:38:58 +0800

    cloth simulation project init

---
+++ ClothSimulation/TexturedModel.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "glad/glad.h"
+#include "Mesh.h"
+#include "Texture.h"
+
+class TexturedModel
+{
+public:
+	TexturedModel(Mesh model, Texture texture)
+		:m_texture(texture), m_model(model){}
+	Texture getTexture() const { return m_texture; }
+	Mesh getModel() const { return m_model; }
+private:
+	Texture m_texture;
+	Mesh m_model;
+};
